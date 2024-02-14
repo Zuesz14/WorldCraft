372484abb6af7b5019e33763e1bbccf2be8db180
Austin-Isaman <132271658+Austin-Isaman@users.noreply.github.com>
2024-02-14 08:55:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
 class Voxel(Button):
-    def __init__(self, position=(0, 0, 0)):
+    def __init__(self, position=(15, 5, 15)):
         super().__init__(
             parent=scene,
             position=position,
             model='cube',
             orgin_y=0.5,
-            texture='white_cube',
+            texture='Tex/woodBlock.jpg',
             color=color.color(0, 0, random.uniform(0.9,1)),
             highlight_color=color.blue)
 
@@ -24,11 +24,13 @@ class Voxel(Button):
 app = Ursina()
 
 e1 = Entity(model="FBX/test item.fbx", texture="Tex/Sniper.png",  scale=.01, position=(1, 1, 1))
+e1 = Entity(model="FBX/Bread.fbx", texture="Tex/texture.png",  scale=.01, position=(10, 1, 10))
 
+for y in range(-2, 1):
+    for z in range(28):
+        for x in range(28):
+            voxel = Voxel(position=(x, y, z))
 
-for z in range(30):
-    for x in range(30):
-        voxel = Voxel(position=(x, 0, z))
 
 player = FirstPersonController()
 sky = Sky()
